bc70dcade8e2937a550ad74722e131fb5151e0cf
Ahnaf Abid <ahnafabid600@gmail.com>
2025-11-25 15:05:48 +0600

    set up mongodb and post

--- index.js
+++ index.js
@@ -1,15 +1,54 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
 const port = process.env.PORT || 3000
 
 //middleware
 app.use(cors())
 app.use(express.json())
 
+const uri = "mongodb+srv://TravelEaseDbUser:zDh5WF4QCoT42of5@cluster0.mrbmxyw.mongodb.net/?appName=Cluster0";
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
 app.get('/', (req, res) => {
   res.send('smart server running')
 })
+
+async function run() {
+  try {
+
+    // Connect to the "sample_mflix" database and access its "movies" collection
+    const database = client.db("travelEase_db");
+    const productCollection = database.collection("allVehicles");
+
+    app.post('/allVehicles', async (req, res)=>{
+        const newProduct = req.body;
+        const result = await productCollection.insertOne(newProduct);
+        res.send(result)
+    })
+    
+
+
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.listen(port, () => {
   console.log(`smart server running on port ${port}`)
