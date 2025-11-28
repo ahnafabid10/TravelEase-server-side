ec5a2ff4aefe09dc41366d8aa3b5c02099ca78e1
Ahnaf Abid <ahnafabid600@gmail.com>
2025-11-29 02:00:48 +0600

    gitingnore

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 3000
 app.use(cors())
 app.use(express.json())
 
-const uri = "mongodb+srv://TravelEaseDbUser:zDh5WF4QCoT42of5@cluster0.mrbmxyw.mongodb.net/?appName=Cluster0";
+const uri = "mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mrbmxyw.mongodb.net/?appName=Cluster0";
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
@@ -91,7 +91,6 @@ res.send(result)
         if(email){
             query.email = email
         }
-
         const cursor = productCollection.find(query)
         const result = await cursor.toArray();
         res.send(result)
@@ -106,11 +105,10 @@ res.send(result)
 
     app.get('/myVehiclePage', async(req,res)=>{
       const email = req.query.email
-      const result = await productCollection.find({
-currentUserEmail : email}).toArray()
+      const result = await productCollection.find({currentUserEmail : email}).toArray()
 res.send(result)
     })
-
+no
     app.put('/allVehicles/:id', async(req, res)=>{
         const id = req.params.id;
         const UpdatedData = req.body
@@ -149,8 +147,8 @@ res.send(result)
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    // await client.db("admin").command({ ping: 1 });
+    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
