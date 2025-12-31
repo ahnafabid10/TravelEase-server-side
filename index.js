80f254f1a6a0e894f9777455759002f767611691
Ahnaf Abid <ahnafabid600@gmail.com>
2026-01-01 03:41:21 +0600

    error fix

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config();
+
 
 const port = process.env.PORT || 3000
 
@@ -9,7 +11,8 @@ const port = process.env.PORT || 3000
 app.use(cors())
 app.use(express.json())
 
-const uri = "mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mrbmxyw.mongodb.net/?appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mrbmxyw.mongodb.net/?appName=Cluster0`;
+
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
@@ -108,7 +111,7 @@ res.send(result)
       const result = await productCollection.find({currentUserEmail : email}).toArray()
 res.send(result)
     })
-no
+
     app.put('/allVehicles/:id', async(req, res)=>{
         const id = req.params.id;
         const UpdatedData = req.body
